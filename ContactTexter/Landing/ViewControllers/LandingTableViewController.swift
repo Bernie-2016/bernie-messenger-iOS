0173be894e3be93a9ae2cdabe6c6234af7b16a9c
Justin Baumgartner <justbaum30@gmail.com>
2016-04-11 01:04:02 -0500

    Flesh out the assignment list cell a bit more

--- ContactTexter/Landing/ViewControllers/LandingTableViewController.swift
+++ ContactTexter/Landing/ViewControllers/LandingTableViewController.swift
@@ -10,4 +10,42 @@ import UIKit
 
 class LandingTableViewController : TableViewController {
     
+    private lazy var dummyCell = LandingTableViewCell.loadFromNib()
+    
+    // MARK: Life cycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 50.0
+        self.tableView.registerReusableCell(LandingTableViewCell.self)
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // MARK: UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: LandingTableViewCell = tableView.dequeueReusableCell(indexPath)
+        // TODO: Configure cell with assignment data
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        // TODO: Configure dummy cell with assignment data
+        return self.dummyCell.calculatedHeight(tableView: tableView)
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // TODO: Go to assignment details page
+    }
+    
 }
