b92c5e3db8c8b953c4845e581123e94649e59ac8
Justin Baumgartner <justbaum30@gmail.com>
2016-04-11 23:55:00 -0500

    Create select contact cell

---
+++ ContactTexter/PerformAssignment/UI/PerformAssignmentSelectContactTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PerformAssignmentSelectContactTableViewCell.swift
+//  ContactTexter
+//
+//  Created by Justin Baumgartner on 4/11/16.
+//  Copyright © 2016 Bernie Sanders 2016. All rights reserved.
+//
+
+import UIKit
+
+class PerformAssignmentSelectContactTableViewCell : UITableViewCell, LoadableFromNib {
+    
+    @IBOutlet private weak var contactNameLabel: UILabel!
+    
+}
