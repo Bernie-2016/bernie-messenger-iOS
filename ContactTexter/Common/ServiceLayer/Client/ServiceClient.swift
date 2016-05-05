d05c5e8dbb7da954a325d93440aaa28895e1afd3
Justin Baumgartner <justbaum30@gmail.com>
2016-05-04 20:50:49 -0500

    Add array mapping

--- ContactTexter/Common/ServiceLayer/Client/ServiceClient.swift
+++ ContactTexter/Common/ServiceLayer/Client/ServiceClient.swift
@@ -15,7 +15,7 @@ final class ServiceClient {
         
         Alamofire.request(serviceEndpoint.URLRequest)
             .validate()
-            .responseObject(keyPath: nil) {
+            .responseObject() {
                 (alamofireResponse: Response<SerializedObject, NSError>) in
                 
                 let result: ServiceResult<SerializedObject>
@@ -32,6 +32,29 @@ final class ServiceClient {
                 
                 completion(result)
             }
+    }
+    
+    static func requestWithEndpoint<SerializedObject: JSONDecodable>(serviceEndpoint: ServiceEndpoint, completion: (ServiceResult<[SerializedObject]>) -> Void) {
+        
+        Alamofire.request(serviceEndpoint.URLRequest)
+            .validate()
+            .responseArray() {
+                (alamofireResponse: Response<[SerializedObject], NSError>) in
+                
+                let result: ServiceResult<[SerializedObject]>
+                switch alamofireResponse.result {
+                case .Success(let responseObject):
+                    result = .Success(responseObject)
+                    
+                case .Failure:
+                    // Response should never be nil, but just in case set it to 500
+                    let httpStatusCode = alamofireResponse.response?.statusCode ?? 500
+                    let error = ServiceError(httpStatusCode: httpStatusCode)
+                    result = .Failure(error)
+                }
+                
+                completion(result)
+        }
     }
     
 }
--- ContactTexter/Common/ServiceLayer/Mapping/FreddyResponseSerializer.swift
+++ ContactTexter/Common/ServiceLayer/Mapping/FreddyResponseSerializer.swift
@@ -17,15 +17,14 @@ extension Request {
      Maps an object from the raw data returned from a service call and executes the specified
      closure after creating the object.
      
-     - parameter keyPath:           The key path of a subset from a response that should be mapped.
      - parameter queue:             The queue to run the service call on.
      - parameter completionHandler: A closure that will execute after attempting to map a
      response object from the service call.
      
      - returns: The Alamofire request.
      */
-    func responseObject<SerializedObject: JSONDecodable>(keyPath keyPath: String? = nil, queue: dispatch_queue_t? = nil, completionHandler:(Response<SerializedObject, NSError>) -> Void) -> Self {
-        let serializer: ResponseSerializer<SerializedObject, NSError> = Request.ObjectResponseSerializer(keyPath)
+    func responseObject<SerializedObject: JSONDecodable>(queue queue: dispatch_queue_t? = nil, completionHandler:(Response<SerializedObject, NSError>) -> Void) -> Self {
+        let serializer: ResponseSerializer<SerializedObject, NSError> = Request.ObjectResponseSerializer()
         return response(queue: queue, responseSerializer: serializer, completionHandler: completionHandler)
     }
     
@@ -41,7 +40,7 @@ extension Request {
      
      - returns: ResponseSerializer used by Alamofire to create a mappable object.
      */
-    static func ObjectResponseSerializer<SerializedObject: JSONDecodable>(keyPath: String?) -> ResponseSerializer<SerializedObject, NSError> {
+    static func ObjectResponseSerializer<SerializedObject: JSONDecodable>() -> ResponseSerializer<SerializedObject, NSError> {
         
         return ResponseSerializer {
             (_, _, data: NSData?, originalError: NSError?) in
@@ -52,24 +51,75 @@ extension Request {
                     return .Failure(error)
                 }
                 
-                guard let data = data else {
+                // Create JSON from data
+                guard let data = data, let json: JSON? = try JSON(data: data) else {
                     let failureReason = "JSON could not be serialized. Input data was nil or zero length."
-                    throw Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
-                }
-                
-                // Create JSON from data and map to key path if provided
-                var json: JSON? = try JSON(data: data)
-                if let keyPath = keyPath {
-                    json = json?[keyPath]
-                }
-                
-                guard json != nil else {
-                    let failureReason = "Key path not valid for response."
                     throw Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
                 }
                 
                 // Map the object and return as success
                 let mappedObject: SerializedObject = try json!.decode()
+                return .Success(mappedObject)
+                
+            } catch {
+                // JSON creation failed
+                return .Failure(error as NSError)
+            }
+        }
+    }
+    
+}
+
+// MARK: Array mapping
+
+extension Request {
+    
+    /**
+     Maps an array from the raw data returned from a service call and executes the specified
+     closure after creating the object.
+     
+     - parameter queue:             The queue to run the service call on.
+     - parameter completionHandler: A closure that will execute after attempting to map a
+     response array from the service call.
+     
+     - returns: The Alamofire request.
+     */
+    func responseArray<SerializedObject: JSONDecodable>(queue queue: dispatch_queue_t? = nil, completionHandler:(Response<[SerializedObject], NSError>) -> Void) -> Self {
+        let serializer: ResponseSerializer<[SerializedObject], NSError> = Request.ArrayResponseSerializer()
+        return response(queue: queue, responseSerializer: serializer, completionHandler: completionHandler)
+    }
+    
+    /**
+     Creates a ResponseSerializer object that is a closure to be executed with the
+     raw data returned from a service call.
+     
+     If the response data is non-empty and can be mapped into a JSON array,
+     mapping will be attempted. If mapping fails for any reason, the error will be
+     passed back.
+     
+     - parameter keyPath: The key path of a subset from a response that should be mapped.
+     
+     - returns: ResponseSerializer used by Alamofire to create a mappable array.
+     */
+    static func ArrayResponseSerializer<SerializedObject: JSONDecodable>() -> ResponseSerializer<[SerializedObject], NSError> {
+        
+        return ResponseSerializer {
+            (_, _, data: NSData?, originalError: NSError?) in
+            
+            do {
+                // For now, not mapping any JSON for errors, return service `NSError`
+                if let error = originalError {
+                    return .Failure(error)
+                }
+                
+                // Create JSON from data
+                guard let data = data, let json: JSON? = try JSON(data: data) else {
+                    let failureReason = "JSON could not be serialized. Input data was nil or zero length."
+                    throw Error.errorWithCode(.JSONSerializationFailed, failureReason: failureReason)
+                }
+                
+                // Map the object and return as success
+                let mappedObject: [SerializedObject] = try json!.arrayOf()
                 return .Success(mappedObject)
                 
             } catch {
